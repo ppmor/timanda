e1b3eed56f8184d4478631c8c0c1c16ebc26b26f
piotr.morzynski <pmorzynski@gmail.com>
2023-12-16 21:30:38 +0100

    creating project structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='timanda',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Lista zależności pakietów, np.:
+        # 'numpy>=1.18.1',
+        # 'pandas>=1.0.1',
+    ],
+    # opcjonalnie inne metadane, jak autor, email, etc.
+)
